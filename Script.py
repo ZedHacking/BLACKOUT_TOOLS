0f7945d7752c05b4b0ca23ea451f2df860f0360b
xxmathexx <101976624+xxmathexx@users.noreply.github.com>
2023-08-01 16:21:09 -0300

    Script.py

--- Script.py
+++ Script.py
@@ -1,5 +1,7 @@
 import socket
 import os
+import requests
+import subprocess
 
 def consultar_ip(site):
     try:
@@ -44,12 +46,46 @@ def consultar_protocolo(site):
     input("\nPressione Enter para continuar...")
     os.system("clear")
 
+def verificar_site_proxy(site):
+    proxies = {
+        "http": "http://proxy.example.com:8080",
+        "https": "https://proxy.example.com:8080"
+    }
+    try:
+        response = requests.get(f"http://{site}", proxies=proxies, timeout=10)
+        if response.status_code == 200:
+            print(f"\n\u2713 O site {site} está online.")
+        else:
+            print(f"\u274C O site {site} está fora do ar.")
+    except requests.exceptions.RequestException:
+        print(f"\u274C O site {site} está fora do ar.")
+    input("\nPressione Enter para continuar...")
+    os.system("clear")
+
+def identificar_vulnerabilidades_sql(site):
+    try:
+        subprocess.run(["sqlmap", "-u", f"http://{site}"])
+    except FileNotFoundError:
+        print("\u274C O sqlmap não está instalado ou não está disponível no sistema.")
+    input("\nPressione Enter para continuar...")
+    os.system("clear")
+
 def creditos():
     os.system("clear")
     print("=====================================")
     print("            \u2705 Créditos \u2705           ")
     print("=====================================")
-    print("\nZed Hacking - Especialista em Segurança Digital")
+    print("\nZed Hacking - Kiba não putinha")
+    input("\nPressione Enter para voltar ao menu...")
+    os.system("clear")
+
+def grupo_telegram():
+    os.system("clear")
+    print("=====================================")
+    print("        \u2605 Grupo do Telegram \u2605      ")
+    print("=====================================")
+    print("\nJunte-se ao nosso grupo do Telegram para receber atualizações, dicas e discussões sobre segurança digital e hacking:")
+    print("Link do grupo: https://t.me/BlackoutTeamOfc")
     input("\nPressione Enter para voltar ao menu...")
     os.system("clear")
 
@@ -62,8 +98,11 @@ if __name__ == "__main__":
         print("1. Consultar IP de um site")
         print("2. Verificar portas abertas de um site")
         print("3. Consultar se o site é UDP ou TCP")
-        print("4. Créditos")
-        print("5. Sair")
+        print("4. Verificar se o site está fora do ar por meio de Proxys")
+        print("5. Identificar vulnerabilidades SQL")
+        print("6. Créditos")
+        print("7. Grupo do Telegram")
+        print("8. Sair")
 
         opcao = input("\nDigite o número da opção desejada: ")
 
@@ -77,8 +116,16 @@ if __name__ == "__main__":
             site = input("Digite o nome do site: ")
             consultar_protocolo(site)
         elif opcao == "4":
-            creditos()
+            site = input("Digite o nome do site: ")
+            verificar_site_proxy(site)
         elif opcao == "5":
+            site = input("Digite o nome do site: ")
+            identificar_vulnerabilidades_sql(site)
+        elif opcao == "6":
+            creditos()
+        elif opcao == "7":
+            grupo_telegram()
+        elif opcao == "8":
             print("Encerrando o programa...")
             break
         else:
