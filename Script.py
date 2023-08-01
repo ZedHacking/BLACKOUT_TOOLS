fcb887fcdeb1a0defc70d7b569baf94a6d3710f7
xxmathexx <101976624+xxmathexx@users.noreply.github.com>
2023-08-01 13:46:30 -0300

    Script.py

--- Script.py
+++ Script.py
@@ -7,14 +7,21 @@ site_name = input("Digite o nome do site: ")
 site_ip = socket.gethostbyname(site_name)
 
 # Verifica as portas abertas no site
+open_ports = []
 for port in range(1, 65535):
     try:
         socket.create_connection((site_ip, port), 2)
-        print(f"A porta {port} está aberta.")
+        open_ports.append(port)
     except socket.error:
         pass
 
-# Verifica se o site usa UDP ou TCP
+# Exibe as 5 primeiras portas abertas
+if len(open_ports) <= 5:
+    print("Portas abertas:", open_ports)
+else:
+    print("Portas abertas:", open_ports[:5])
+
+# Identifica se o site usa TCP ou UDP
 if socket.getservbyport(80, "tcp") == site_ip:
     print(f"O site usa TCP.")
 else:
